6d96246107cd72b9f659466c59bd911c5217f247
AJ-505 <abasiono.mbat@gmail.com>
2025-01-29 15:15:21 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+let [home, sibas, e_learning, student_portal, sst, smc] = [document.getElementById("home"),
+     document.getElementById("sibas"),
+     document.getElementById("e-learning"),
+     document.getElementById("student-portal"),
+     document.getElementById("sst"),
+     document.getElementById("smc")
+    ];
+
+function show_smc() {
+    smc.style.display = "block";
+    home.style.display = "none";
+    sibas.style.display = "none";
+    e_learning.style.display = "none";
+    student_portal.style.display = "none";
+    sst.style.display = "none";
+}
+
+function show_sst() {
+    sst.style.display = "block";
+    home.style.display = "none";
+    sibas.style.display = "none";
+    e_learning.style.display = "none";
+    student_portal.style.display = "none";
+    smc.style.display = "none";
+}
+
+function show_student_portal() {
+    student_portal.style.display = "block";
+    home.style.display = "none";
+    sibas.style.display = "none";
+    e_learning.style.display = "none";
+    smc.style.display = "none";
+    sst.style.display = "none";
+}
+
+function show_e_learning() {
+    e_learning.style.display = "block";
+    home.style.display = "none";
+    sibas.style.display = "none";
+    smc.style.display = "none";
+    student_portal.style.display = "none";
+    sst.style.display = "none";
+}
+
+function show_sibas() {
+    sibas.style.display = "block";
+    home.style.display = "none";
+    smc.style.display = "none";
+    e_learning.style.display = "none";
+    student_portal.style.display = "none";
+    sst.style.display = "none";
+}
+
+function show_home() {
+    home.style.display = "block";
+    smc.style.display = "none";
+    sibas.style.display = "none";
+    e_learning.style.display = "none";
+    student_portal.style.display = "none";
+    sst.style.display = "none";
+}
