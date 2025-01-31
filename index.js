abd72785324e5489d0a933a51db11852b9b11da0
AJ-505 <abasiono.mbat@gmail.com>
2025-01-31 02:39:54 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,97 +1,40 @@
-let [home, sibas, e_learning, student_portal, sst, smc, library, cafeteria] = [document.getElementById("home"),
-     document.getElementById("sibas"),
-     document.getElementById("e-learning"),
-     document.getElementById("student-portal"),
-     document.getElementById("sst"),
-     document.getElementById("smc"), 
-     document.getElementById("library"),
-     document.getElementById("cafeteria")
-    ];
+function show_site(name) {
+    const iframes = document.querySelectorAll("iframe");
+    let target_frame = document.getElementById(name);
+    iframes.forEach(iframe => {
+        iframe.style.display = "none";
+    })
+    target_frame.style.display = "block";
+}
 
 function show_smc() {
-    smc.style.display = "block";
-    home.style.display = "none";
-    sibas.style.display = "none";
-    e_learning.style.display = "none";
-    student_portal.style.display = "none";
-    sst.style.display = "none";
-    library.style.display = "none";
-    cafeteria.style.display = "none";
+    show_site("smc");
 }
 
 function show_sst() {
-    sst.style.display = "block";
-    home.style.display = "none";
-    sibas.style.display = "none";
-    e_learning.style.display = "none";
-    student_portal.style.display = "none";
-    smc.style.display = "none";
-    library.style.display = "none";
-    cafeteria.style.display = "none";
+    show_site("sst");
 }
 
-function show_student_portal() {
-    student_portal.style.display = "block";
-    home.style.display = "none";
-    sibas.style.display = "none";
-    e_learning.style.display = "none";
-    smc.style.display = "none";
-    sst.style.display = "none";
-    library.style.display = "none";
-    cafeteria.style.display = "none";
+function show_tic() {
+    show_site("tic");
 }
 
 function show_e_learning() {
-    e_learning.style.display = "block";
-    home.style.display = "none";
-    sibas.style.display = "none";
-    smc.style.display = "none";
-    student_portal.style.display = "none";
-    sst.style.display = "none";
-    library.style.display = "none";
-    cafeteria.style.display = "none";
+    show_site("e-learning");
 }
 
 function show_sibas() {
-    sibas.style.display = "block";
-    home.style.display = "none";
-    smc.style.display = "none";
-    e_learning.style.display = "none";
-    student_portal.style.display = "none";
-    sst.style.display = "none";
-    library.style.display = "none";
-    cafeteria.style.display = "none";
+    show_site("sibas");
 }
 
 function show_home() {
-    home.style.display = "block";
-    smc.style.display = "none";
-    sibas.style.display = "none";
-    e_learning.style.display = "none";
-    student_portal.style.display = "none";
-    sst.style.display = "none";
-    library.style.display = "none";
-    cafeteria.style.display = "none";
+    show_site("home");
 }
 
 function show_library() {
-    library.style.display = "block";
-    home.style.display = "none";
-    sibas.style.display = "none";
-    e_learning.style.display = "none";
-    student_portal.style.display = "none";
-    smc.style.display = "none";
-    sst.style.display = "none";
-    cafeteria.style.display = "none";
+    show_site("library");
 }
 
 function show_cafeteria() {
-    cafeteria.style.display = "block";
-    home.style.display = "none";
-    sibas.style.display = "none";
-    e_learning.style.display = "none";
-    student_portal.style.display = "none";
-    smc.style.display = "none";
-    sst.style.display = "none";
-    library.style.display = "none";
+    show_site("cafeteria");
 }
